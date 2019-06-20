ede88ea94dc5b04b08043874a164100b968fd3b0
Jaid <jaid.jsx@gmail.com>
2019-06-20 23:30:27 +0200

    Slightly improved documentation
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
  * Returns the number of seconds passed since Unix epoch (01 January 1970)
  * @example
  * import epochSeconds from "epoch-seconds"
- * epochSeconds()
- * // 1549410770
+ * const result = epochSeconds()
+ * result === 1549410770
  * @returns {number} Seconds since epoch
  */
 export default () => Math.floor(Date.now() / 1000)
