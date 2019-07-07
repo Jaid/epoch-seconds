0b8a9290ef643417f7cf94070e6e544ab397e382
Jaid <jaid.jsx@gmail.com>
2019-07-07 17:39:23 +0200

    doc: Changed order of JSDoc annotations for better readability
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 
 /**
  * Returns the number of seconds passed since Unix epoch (01 January 1970)
+ * @function
+ * @returns {number} Seconds since epoch
  * @example
  * import epochSeconds from "epoch-seconds"
  * const result = epochSeconds()
  * result === 1549410770
- * @function
- * @returns {number} Seconds since epoch
  */
 export default () => Math.floor(Date.now() / 1000)
