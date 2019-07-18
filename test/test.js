a7212f68df060ee596eb2f10cffa956674572057
Jaid <jaid.jsx@gmail.com>
2019-07-18 15:16:10 +0200

    Fixed comment align
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ import path from "path"
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
-   * @type { import("../src") }
-   */
+ * @type { import("../src") }
+ */
 const {default: epochSeconds} = indexModule
 
 it("should run", () => {
