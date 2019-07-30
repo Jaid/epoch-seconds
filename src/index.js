cf8965586ccb5652670c00414997fe110d65f399
Jaid <jaid.jsx@gmail.com>
2019-07-30 23:20:16 +0200

    feat: Added parameter `compareValue`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 /** @module epoch-seconds */
 
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
  * @function
- * @returns {number} Seconds since epoch
+ * @param {number} [compareValue]
+ * @returns {number} Seconds passed since Unix epoch (01 January 1970)
  * @example
  * import epochSeconds from "epoch-seconds"
  * const result = epochSeconds()
  * result === 1549410770
+ * setTimeout(() => {
+ *   const result2 = epochSeconds(result)
+ *   result2 === 3
+ * }, 3000)
  */
-export default () => Math.floor(Date.now() / 1000)
+export default compareValue => {
+  const seconds = Math.floor(Date.now() / 1000)
+  if (compareValue === undefined) {
+    return seconds
+  } else {
+    return seconds - compareValue
+  }
+}
