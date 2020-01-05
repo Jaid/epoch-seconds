ce6f0022fbe2cd18b27d7edbe371e6052f6c018f
Jaid <jaid.jsx@gmail.com>
2020-01-05 06:19:24 +0100

    Updated coverage gathering method
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: "node",
   coverageDirectory: "dist/jest/coverage",
-  collectCoverageFrom: ["src/**"],
+  collectCoverageFrom: ["<rootDir>/dist/package/development/**"],
   testPathIgnorePatterns: [
     "/node_modules/",
     "/dist/",
