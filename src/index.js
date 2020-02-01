4a5d540baa0adf9fde23c46826c40bfdd155dd8b
Jaid <jaid.jsx@gmail.com>
2020-02-01 03:16:42 +0100

    Simplified JSDoc comment
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 /** @module epoch-seconds */
 
 /**
- * @function
  * @param {number} [compareValue]
  * @returns {number} Seconds passed since Unix epoch (01 January 1970)
- * @example
- * import epochSeconds from "epoch-seconds"
- * const result = epochSeconds()
- * result === 1549410770
- * setTimeout(() => {
- *   const result2 = epochSeconds(result)
- *   result2 === 3
- * }, 3000)
  */
 export default compareValue => {
   const seconds = Math.floor(Date.now() / 1000)
