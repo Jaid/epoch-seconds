5abfc3499cd7eb825879af50d616a592e13c8f11
Jaid <jaid.jsx@gmail.com>
2020-03-16 05:56:24 +0100

    feat: Removed first argument to keep this package simple
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 /** @module epoch-seconds */
 
 /**
- * @param {number} [compareValue]
  * @returns {number} Seconds passed since Unix epoch (01 January 1970)
  */
-export default compareValue => {
-  const seconds = Math.floor(Date.now() / 1000)
-  if (compareValue === undefined) {
-    return seconds
-  } else {
-    return seconds - compareValue
-  }
+export default () => {
+  return Math.floor(Date.now() / 1000)
 }
