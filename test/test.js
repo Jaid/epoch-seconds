086ed1fe1284a093ba1cc0d2a0504686c56760fe
Jaid <jaid.jsx@gmail.com>
2020-04-18 05:22:30 +0200

    Updated test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+import "@types/jest"
+
 import path from "path"
 
 const indexModule = require(process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src"))
@@ -9,5 +11,6 @@ const {default: epochSeconds} = indexModule
 
 it("should run", () => {
   const result = epochSeconds()
-  expect(result).toBeGreaterThan(1549410770)
+  expect(result).toBeGreaterThan(1514764800) // Jan 01 2018 or greater
+  expect(result).toBeLessThan(3786912000) // Jan 01 2090 or lower
 })
