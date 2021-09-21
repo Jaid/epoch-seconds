df945bd938cc6b9e61ccde8fd201750defdb9006
Jaid <jaid.jsx@gmail.com>
2021-09-21 15:48:04 +0200

    Applied ESLint code fixes

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
+import path from "node:path"
+
 import {expect, it} from "@jest/globals"
-import path from "path"
 
 const indexModule = require(process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src"))
 
@@ -10,6 +11,6 @@ const {default: epochSeconds} = indexModule
 
 it("should run", () => {
   const result = epochSeconds()
-  expect(result).toBeGreaterThan(1514764800) // Jan 01 2018 or greater
-  expect(result).toBeLessThan(3786912000) // Jan 01 2090 or lower
+  expect(result).toBeGreaterThan(1_514_764_800) // Jan 01 2018 or greater
+  expect(result).toBeLessThan(3_786_912_000) // Jan 01 2090 or lower
 })
