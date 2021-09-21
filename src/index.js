0d8d5deff89158ba238433449243d256ae2c0564
Jaid <jaid.jsx@gmail.com>
2021-09-21 15:53:03 +0200

    improve: Changed function type to arrow
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 /**
  * @return {number} Seconds passed since Unix epoch (01 January 1970)
  */
-export default function () {
+export default () => {
   const msSinceUnixEpoch = Date.now()
   const secondsSinceUnixEpoch = Math.floor(msSinceUnixEpoch / 1000)
   return secondsSinceUnixEpoch
