548f1c1d4c746119ca715f9f8f69b973d6e79c67
Jaid <6216144+Jaid@users.noreply.github.com>
2023-10-03 03:31:29 +0200

    Removed legacy files
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- etc/webpack-config-jaid/OutputConfigPlugin.ts
+++
@@ -1,9 +0,0 @@
-import type {Compiler, WebpackPluginInstance} from 'webpack'
-
-export class OutputConfigPlugin implements WebpackPluginInstance {
-  apply(compiler: Compiler) {
-    compiler.hooks.beforeCompile.tap(`OutputConfigPlugin`, () => {
-      console.dir(compiler.options)
-    })
-  }
-}
