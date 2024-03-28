c241aaa55e5192196b7ebe710c7e227db8bfac9b
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-28 23:23:04 +0100

    Improved typing
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import type {NonNegative, NonNegativeInteger} from 'type-fest'
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now
  * @example const now = epochSeconds()
  */
-const epochSeconds = () => {
+const epochSeconds = (): NonNegativeInteger<number> => {
   const msSinceUnixEpoch = Date.now()
   const secondsSinceUnixEpoch = Math.trunc(msSinceUnixEpoch / 1000)
   return secondsSinceUnixEpoch as NonNegativeInteger<number>
